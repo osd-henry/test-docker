9dd2b4cf14b5796ad01468d2304f6e8987395e38
Hung Luu <hungluu2106@gmail.com>
2019-05-22 13:55:24 +0700

    Fix artifact config 3

--- scripts/build_artifacts.sh
+++ scripts/build_artifacts.sh
@@ -2,6 +2,7 @@
 ARTIFACT_DIR="artifacts"
 FILES=( ".dockerignore" "docker-compose.yml" "docker-compose.prod.yml" "Dockerfile" "index.js" "package.json" )
 
+cd ..
 mkdir -p "$ARTIFACT_DIR"
 for i in "${FILES[@]}"
 do
