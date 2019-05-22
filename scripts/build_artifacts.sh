bd44d7651d9a266775665eabb4c04643c7d300be
Hung Luu <hungluu2106@gmail.com>
2019-05-22 14:48:32 +0700

    Fix artifact config 8

--- scripts/build_artifacts.sh
+++ scripts/build_artifacts.sh
@@ -3,8 +3,10 @@ ARTIFACT_DIR="artifacts"
 FILES=( ".dockerignore" "docker-compose.yml" "docker-compose.prod.yml" "Dockerfile" "index.js" "package.json" )
 
 cd ..
+echo "Ensure folder $ARTIFACT_DIR"
 mkdir -p "$ARTIFACT_DIR"
 for i in "${FILES[@]}"
 do
+    echo "Copying source/$i..."
     cp "source/$i" "$ARTIFACT_DIR"
 done
