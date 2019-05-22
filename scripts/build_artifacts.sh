b27fe720beb09779002de99f72765655e506b0af
Hung Luu <hungluu2106@gmail.com>
2019-05-22 11:40:46 +0700

    Add actifacts build

---
+++ scripts/build_artifacts.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+ARTIFACT_DIR="artifacts"
+FILES=( ".dockerignore" "docker-compose.yml" "docker-compose.prod.yml" "Dockerfile" "index.js" "package.json" )
+
+mkdir -p "$ARTIFACT_DIR"
+for i in "${FILES[@]}"
+do
+    cp $i "$ARTIFACT_DIR"
+done
