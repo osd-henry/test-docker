4e427484a7a23be99a08c8b4ef95379629ca083f
Hung Luu <hungluu2106@gmail.com>
2019-05-22 15:05:48 +0700

    Fix artifact config 10

--- scripts/build_artifacts.sh
+++ scripts/build_artifacts.sh
@@ -3,12 +3,12 @@ ARTIFACT_DIR="artifacts"
 FILES=( ".dockerignore" "docker-compose.yml" "docker-compose.prod.yml" "Dockerfile" "index.js" "package.json" )
 
 cd ..
-echo "Ensure folder $ARTIFACT_DIR"
+echo "Copying to artifact folder: $ARTIFACT_DIR ..."
 mkdir -p "$ARTIFACT_DIR"
 for i in "${FILES[@]}"
 do
-    echo "Copying source/$i..."
     cp "source/$i" "$ARTIFACT_DIR"
 done
 
-ls "$ARTIFACT_DIR"
+echo "$PWD/$ARTIFACT_DIR:"
+ls "$ARTIFACT_DIR" | sed "s/^/ - /"
