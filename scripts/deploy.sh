26c607c5c5fad9615379cec1e430b1643f429c24
Hung Luu <hungluu2106@gmail.com>
2019-05-22 11:11:36 +0700

    Separate deploy config yml file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-DEPLOY_HOST="$3"
-DEPLOY_REPO="$1"
-DEPLOY_DIR="$5"
-SSH_KEY="$7"
-COMMAND="mv $DEPLOY_DIR $DEPLOY_DIR.bak && rm -rf $DEPLOY_DIR && git clone $DEPLOY_REPO $DEPLOY_DIR && cd $DEPLOY_DIR && sudo docker-compose up --build -d"
+DEPLOY_HOST="root@104.248.157.25"
+DEPLOY_REPO="git@github.com:osd-henry/test-docker.git"
+DEPLOY_DIR="app"
+SSH_KEY="/var/go/.ssh/webminar_id_rsa"
+UPDATE_SOURCE="mv $DEPLOY_DIR $DEPLOY_DIR.bak && rm -rf $DEPLOY_DIR && git clone $DEPLOY_REPO $DEPLOY_DIR"
+START_SERVER="sudo docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d"
+COMMANDS="$UPDATE_SOURCE && cd $DEPLOY_DIR && $START_SERVER"
 
-ssh -i "$SSH_KEY" "$DEPLOY_HOST" "$COMMAND"
+ssh -i "$SSH_KEY" "$DEPLOY_HOST" "$COMMANDS"
